3872c7b5ca7feefc45b9f96d7b06da24b66fad07
HtetArkarOo <yuki77475@gmail.com>
2021-08-30 10:09:42 +0630

    Circle Calculations

---
+++ area.py
@@ -0,0 +1,6 @@
+from math import pi
+
+r = float(input("Enter the radius of circle: "))
+A = pi * (r**2)
+
+print(f"Area of the circle is {A}.")
---
+++ volume.py
@@ -0,0 +1,8 @@
+from math import pi
+
+r = float(input("Enter the radius of circle: "))
+
+V = pi * (r**3) * (4/3)
+
+print(f"Volume of the circle is {V}.")
+
